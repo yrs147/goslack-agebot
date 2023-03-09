8359252f2e46dc020541ce6d933b9732a1c911f0
yrs147 <yashraj14700728@gmail.com>
2023-03-09 22:00:04 +0530

    Fixed Errors

--- main.go
+++ main.go
@@ -1,17 +1,19 @@
-package main 
+package main
 
-import(
+import (
 	"context"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+
 	"github.com/shomali11/slacker"
 )
 
-func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent){
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
 	for event := range analyticsChannel {
 		fmt.Println("Command Events")
-		fmt.Println(event.TimeStamp)
+		fmt.Println(event.Timestamp)
 		fmt.Println(event.Command)
 		fmt.Println(event.Parameters)
 		fmt.Println(event.Event)
@@ -19,34 +21,34 @@ func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent){
 	}
 }
 
-func main(){
-	os.Setenv("SLACK_BOT_TOKEN","")
-	os.Setenv("SLACK_APP_TOKEN","")
+func main() {
+	os.Setenv("SLACK_BOT_TOKEN", "")
+	os.Setenv("SLACK_APP_TOKEN", "")
 
 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
 
-	go printCommandEvents(bot.printCommandEvents())
+	go printCommandEvents(bot.CommandEvents())
 
-	bot.Command("my yob is <year>", &slacker.CommandDefinition){
+	bot.Command("my yob is <year>", &slacker.CommandDefinition{
 		Description: "yob Calculator",
-		Example: "my yob is 2005" ,
-		Handler: func(botCtx slacker.BotContext, request slacker.Request , response slacker.ResponseWriter){
+		Examples: []string{"my yob is 2005"} ,
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
 			year := request.Param("year")
 			yob, err := strconv.Atoi(year)
-			if err != nil{
+			if err != nil {
 				println("error")
 			}
-			age := 2023-yob
+			age := 2023 - yob
 			r := fmt.Sprintf("Age is %d", age)
 			response.Reply(r)
-		}
-	}
+		},
+	})
 
-	ctx , cancel := context.WithCancel(context.Background())
+	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	
+
 	err := bot.Listen(ctx)
-	if err !=nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 }
